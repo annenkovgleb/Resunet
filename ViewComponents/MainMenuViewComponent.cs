f358aeb193d66b52de609f9449361abb9f59353f
mwah <annenkovgleb@proton.me>
2024-11-15 21:00:26 +0300

    Main menu and Components

---
+++ ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resunet.BL.Auth;
+
+namespace Resunet.ViewComponents
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly ICurrentUser currentUser;
+        public MainMenuViewComponent(ICurrentUser currentUser)
+        {
+            this.currentUser = currentUser;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            bool isLoggedIn = await currentUser.IsLoggedIn();
+            return View("Index", isLoggedIn);
+        }
+    }
+}
